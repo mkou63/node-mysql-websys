e71d2f3bd29fdae599ab33496ba8a234e79fdf4f
mkou63 <j2238@matsue-ct.ac.jp>
2025-06-19 05:37:04 +0000

    13の最初くらい

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'todo_app'
+});
+
+let todos = [];
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'ToDo App',
+    todos: todos,
+  });
+});
+
+router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
+  const todo = req.body.add;
+  todos.push(todo);
+  res.redirect('/');
 });
 
 module.exports = router;
